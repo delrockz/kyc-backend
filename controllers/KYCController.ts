6a6d3a84405c6bb0409e2ded634f2b7636d38e01
delrockz <71059214+delrockz@users.noreply.github.com>
2025-02-11 22:09:37 +0530

    feat: Admin dashboard - KYC overview, list, approve & reject APIs & admin auth middleware

--- controllers/KYCController.ts
+++ controllers/KYCController.ts
@@ -3,6 +3,7 @@ import { IRequestWithUser } from '../interfaces/IRequestWithUser'
 import KycApplication from '../models/KycApplication'
 import multer from 'multer'
 import { uploadToS3 } from '../utils/uploadFile'
+import User from '../models/User'
 
 const storage = multer.memoryStorage()
 const upload = multer({ storage }).single('idDocumentImage')
@@ -37,4 +38,66 @@ export const submitKyc = async (req: IRequestWithUser, res: Response, next: () =
       return res.status(500).json({ error: error.message })
     }
   })
+}
+
+export const getKycApplications = async (req: Request, res: Response) => {
+  const { status } = req.query
+
+  const filter: any = {}
+  if (status) filter.status = status
+
+  try {
+    const documents = await KycApplication.find(filter).populate('user', 'email')
+    return res.status(200).json({ data: documents })
+  } catch (err: any) {
+    return res.status(500).json({ error: err.message })
+  }
+}
+
+export const approveKyc = async (req: Request, res: Response) => {
+  const { id } = req.params
+
+  try {
+    await KycApplication.findByIdAndUpdate(id, { status: 'approved' })
+    return res.status(200).json({ message: 'KYC approved' })
+  } catch (err: any) {
+    return res.status(500).json({ error: err.message })
+  }
+}
+
+export const rejectKyc = async (req: Request, res: Response) => {
+  const { id } = req.params
+  try {
+    await KycApplication.findByIdAndUpdate(id, { status: 'rejected' })
+    return res.status(200).json({ message: 'KYC rejected' })
+  } catch (err: any) {
+    return res.status(500).json({ error: err.message })
+  }
+}
+
+export const getKycDashboard = async (req: Request, res: Response) => {
+  try {
+    const [nUsers, kycStats] = await Promise.all([
+      User.countDocuments(),
+      KycApplication.aggregate([
+        {
+          $group: {
+            _id: '$status',
+            count: { $sum: 1 }
+          }
+        }
+      ])
+    ])
+
+    return res.status(200).json({
+      data: {
+        nUsers: nUsers,
+        nPending: kycStats.find((s) => s._id === 'pending')?.count || 0,
+        nApproved: kycStats.find((s) => s._id === 'approved')?.count || 0,
+        nRejected: kycStats.find((s) => s._id === 'rejected')?.count || 0
+      }
+    })
+  } catch (err: any) {
+    return res.status(500).json({ error: err.message })
+  }
 }
--- routes.ts
+++ routes.ts
@@ -1,6 +1,7 @@
-import { submitKyc } from './controllers/KYCController'
 import { authenticate } from './middleware/auth'
+import { approveKyc, getKycDashboard, getKycApplications, rejectKyc, submitKyc } from './controllers/KYCController'
 import { loginUser, signupUser } from './controllers/UserController'
+import { adminAuthenticate } from './middleware/adminAuth'
 
 const express = require('express')
 const router = express.Router()
@@ -11,5 +12,11 @@ router.post('/login', loginUser)
 
 // User APIs
 router.post('/submitKyc', authenticate, submitKyc)
+
+// Admin APIs
+router.get('/kycapplications', adminAuthenticate, getKycApplications)
+router.get('/kycdashboard', adminAuthenticate, getKycDashboard)
+router.put('/approveKyc/:id', adminAuthenticate, approveKyc)
+router.put('/rejectKyc/:id', adminAuthenticate, rejectKyc)
 
 export default router
